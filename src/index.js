f455bae7ed67505eaec7436ea37d81797bd9d3ac
LiB-z <365rik365@rambler.ru>
2022-08-13 00:00:07 +0200

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codeArr = expr.split("**********");
+    let result = [];
+
+    //step first: split EXPR into parts & translate numbers into symbols
+    codeArr.forEach(el => {
+        let counter = Math.floor(el.length/10); 
+        let words = el;
+        let currentWord = "";
+        let decodeArr = [];
+        
+        for (let i = 0; i < counter; i++) {
+            let transformedWord = "";
+            currentWord = words.slice(i*10, 10 + i*10);
+            currentWord.replace(/(10)|(11)|(00)/g, (match, p1, p2, p3) => {
+                if(p1) {transformedWord+="."};
+                if(p2) {transformedWord+="-"};
+                if(p3) {transformedWord+=""};
+            })
+            decodeArr.push(transformedWord);
+        }
+
+        //step second: split EXPR into parts & translate numbers into symbols
+        decodeArr.forEach((el, index) => {
+            decodeArr[index] = MORSE_TABLE[el]
+        })
+        decodeArr = decodeArr.join('');
+        result.push(decodeArr);
+              
+    });
+    
+    //step third: connect people c:
+    result = result.join(" ");
+    return result;
 }
 
 module.exports = {
